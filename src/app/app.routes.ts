cc631e747f4c7e28eaac93e2b0d65cf4c22d3aaa
Nicolas Riano <benicolas11@hotmail.com>
2024-02-08 14:43:44 -0500

    se inicia proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ConfiguracionComponent } from './paginas/configuracion/configuracion.component';
+import { InformacionComponent } from './paginas/informacion/informacion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: InicioComponent},
+    {path: 'config', component: ConfiguracionComponent},
+    {path: 'info*:id', component: InformacionComponent},
+    {path: '', redirectTo: '', pathMatch:'full'}
+];
